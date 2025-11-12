e5c25e056ff02850536606de9fe1e42fde2aafd7
huberbas <huberbas@posteo.net>
2025-11-12 14:55:44 +0100

    add lazyvim

---
+++ .config/nvim/lua/plugins/user.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true,
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
+}
