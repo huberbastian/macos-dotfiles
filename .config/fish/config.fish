2136d7ca51041acdf0578c7fcd4efa95f3a8fbf1
huberbas <huberbas@posteo.net>
2025-11-30 22:06:35 +0100

    add fish shell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    set -U fish_greeting
+
+    fish_config theme choose "Catppuccin Mocha"
+
+    set -gx EDITOR nvim
+    set -gx JAVA_HOME /opt/homebrew/opt/openjdk
+    set -gx PNPM_HOME "$HOME/Library/pnpm"
+
+    fish_add_path "$HOME/bin"
+    fish_add_path "$HOMO/.local/bin"
+    fish_add_path "$JAVA_HOME/bin"
+    fish_add_path "$PNPM_HOME"
+
+    alias vi="nvim"
+    alias svi="sudo nvim"
+    alias ls="eza --icons=always"
+    alias la="eza -la --icons=always"
+
+    set -gx STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
+    starship init fish | source
+end
